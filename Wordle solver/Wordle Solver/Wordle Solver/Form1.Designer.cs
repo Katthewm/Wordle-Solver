df5fb1f01db429d0e4e450ed64595bff45c71029
Katthewm <34632839+Katthewm@users.noreply.github.com>
2022-02-17 20:44:35 -0600

    wordle solver v1

---
+++ Wordle solver/Wordle Solver/Wordle Solver/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Wordle_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Delete Letter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Contains Letter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(112, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AccessibleDescription = "0";
+            this.textBox3.AccessibleName = "";
+            this.textBox3.Location = new System.Drawing.Point(6, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(21, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.Rightposition);
+            // 
+            // textBox4
+            // 
+            this.textBox4.AccessibleDescription = "1";
+            this.textBox4.AccessibleName = "";
+            this.textBox4.Location = new System.Drawing.Point(33, 116);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(21, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextChanged += new System.EventHandler(this.Rightposition);
+            // 
+            // textBox5
+            // 
+            this.textBox5.AccessibleDescription = "2";
+            this.textBox5.AccessibleName = "";
+            this.textBox5.Location = new System.Drawing.Point(60, 116);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(21, 20);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.TextChanged += new System.EventHandler(this.Rightposition);
+            // 
+            // textBox6
+            // 
+            this.textBox6.AccessibleDescription = "3";
+            this.textBox6.AccessibleName = "";
+            this.textBox6.Location = new System.Drawing.Point(87, 116);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(21, 20);
+            this.textBox6.TabIndex = 8;
+            this.textBox6.TextChanged += new System.EventHandler(this.Rightposition);
+            // 
+            // textBox7
+            // 
+            this.textBox7.AccessibleDescription = "4";
+            this.textBox7.AccessibleName = "";
+            this.textBox7.Location = new System.Drawing.Point(112, 116);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(21, 20);
+            this.textBox7.TabIndex = 9;
+            this.textBox7.TextChanged += new System.EventHandler(this.Rightposition);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(227, 14);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox8.Size = new System.Drawing.Size(152, 189);
+            this.textBox8.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Right POS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Wrong POS";
+            // 
+            // textBox9
+            // 
+            this.textBox9.AccessibleName = "4";
+            this.textBox9.Location = new System.Drawing.Point(112, 142);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(21, 20);
+            this.textBox9.TabIndex = 16;
+            this.textBox9.TextChanged += new System.EventHandler(this.Wrongposition);
+            // 
+            // textBox10
+            // 
+            this.textBox10.AccessibleName = "3";
+            this.textBox10.Location = new System.Drawing.Point(87, 142);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(21, 20);
+            this.textBox10.TabIndex = 15;
+            this.textBox10.TextChanged += new System.EventHandler(this.Wrongposition);
+            // 
+            // textBox11
+            // 
+            this.textBox11.AccessibleName = "2";
+            this.textBox11.Location = new System.Drawing.Point(60, 142);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(21, 20);
+            this.textBox11.TabIndex = 14;
+            this.textBox11.TextChanged += new System.EventHandler(this.Wrongposition);
+            // 
+            // textBox12
+            // 
+            this.textBox12.AccessibleName = "1";
+            this.textBox12.Location = new System.Drawing.Point(33, 142);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(21, 20);
+            this.textBox12.TabIndex = 13;
+            this.textBox12.TextChanged += new System.EventHandler(this.Wrongposition);
+            // 
+            // textBox13
+            // 
+            this.textBox13.AccessibleName = "0";
+            this.textBox13.Location = new System.Drawing.Point(6, 142);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(21, 20);
+            this.textBox13.TabIndex = 12;
+            this.textBox13.TextChanged += new System.EventHandler(this.Wrongposition);
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(6, 177);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.ReadOnly = true;
+            this.textBox14.Size = new System.Drawing.Size(100, 20);
+            this.textBox14.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 271);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+    }
+}
+
---
+++ Wordle solver/Wordle Solver/Wordle Solver/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wordle_Solver
+{
+    public partial class Form1 : Form
+    {
+        List<string> words = new List<string>();
+        List<string> workingset = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            loadfile();
+        }
+
+        private void loadfile()
+        {
+            string[] lines = File.ReadAllLines(@"C:/Users/mattk/Desktop/test.txt");
+
+            foreach (string line in lines)
+            {
+                words.Add(line);
+            }
+            textBox14.Text = words.Count.ToString();
+            words.Sort();
+            workingset = words;
+            printoutwords();
+        }
+
+        private void printoutwords()
+        {
+            string line = "";
+            textBox8.Text = "";
+            foreach(string word in workingset)
+            {
+                line=line+word+System.Environment.NewLine;
+            }
+            textBox8.Text = line;
+            textBox14.Text=words.Count.ToString();
+        }
+
+        //delete a letter
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length != 1)
+            {
+                return;
+            }
+            for(int i=0; i< workingset.Count; i++)
+            {
+                if(workingset[i].Contains(textBox1.Text)==true)
+                {
+                    workingset.RemoveAt(i);
+                }
+            }
+            printoutwords();
+        }
+
+        //contains a letter
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length != 1)
+            {
+                return;
+            }
+            for (int i = 0; i < workingset.Count; i++)
+            {
+                if (workingset[i].Contains(textBox2.Text)==false)
+                {
+                    workingset.RemoveAt(i);
+                }
+            }
+            printoutwords();
+        }
+        //reset working set
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<string> workingset = words;
+            printoutwords();
+        }
+
+        private void Rightposition(object sender, EventArgs e)
+        {
+            TextBox textbox = (TextBox)sender;
+            if (textbox.Text.Length != 1)
+            {
+                return;
+            }
+            for (int i = 0; i < workingset.Count; i++)
+            {
+                if (workingset[i][Int32.Parse(textbox.AccessibleDescription)]+"" != textbox.Text || workingset[i].Contains(textbox.Text) == false)
+                {
+                    workingset.RemoveAt(i);
+                }
+            }
+            printoutwords();
+        }
+
+        private void Wrongposition(object sender, EventArgs e)
+        {
+            TextBox textbox = (TextBox)sender;
+            if (textbox.Text.Length != 1)
+            {
+                return;
+            }
+            for (int i = 0; i < workingset.Count; i++)
+            {
+                if (workingset[i][Int32.Parse(textbox.AccessibleName)] + "" == textbox.Text || workingset[i].Contains(textbox.Text) == false)
+                {
+                    workingset.RemoveAt(i);
+                }
+            }
+            printoutwords();
+        }
+    }
+}
